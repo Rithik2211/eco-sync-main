80610f5971aaa83fdbedb08eaa3e183d81414bec
Rithik2211 <rithikpranao22@gmail.com>
2024-11-09 01:04:14 +0530

    Removed Search component and its related code from NavBar.tsx.

--- app/UI/NavBar.tsx
+++ app/UI/NavBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Search } from 'lucide-react';
 import NavButton from '../utils/NavButton';
 import DarkModeToggle, { ImageToggle } from '../utils/DarkModeToggle';
 
@@ -11,15 +10,6 @@ const NavBar = () => {
           <ImageToggle img1='/logo-light.png' img2='/logo-dark.png' alt='logo' width={120} height={120}/>
         </div>
         <div className='flex justify-around items-center w-[300px] h-[40px] flex-wrap'>
-          {/* Search Bar */}
-          {/* <div className="flex items-center rounded-[8px] border bg-[#E5E8EB] px-4 py-1">
-            <Search className="h-5 w-5 text-gray-600" />
-            <input
-              type="text"
-              placeholder="Search"
-              className="ml-2 w-full bg-transparent outline-none placeholder:text-gray-600"
-            />
-          </div> */}
           {/* Login / SignIn */}
           <div className='flex items-center space-x-4'>
             <DarkModeToggle />
